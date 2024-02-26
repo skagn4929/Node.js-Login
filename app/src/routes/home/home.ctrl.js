60949b32dac107561e1a1cb5e0ac55c40f05152e
기남후 <skagn4929@naver.com>
2024-02-26 17:53:49 +0900

    로그인 인증 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,43 @@
 "use strict";
 
-const hello = (req, res) => {
-  res.render("home/index");
+const output = {
+  hello: (req, res) => {
+    res.render("home/index");
+  },
+
+  login: (req, res) => {
+    res.render("home/login");
+  },
 };
 
-const login = (req, res) => {
-  res.render("home/login");
+const users = {
+  id: ["test", "ki", "noh"],
+  psword: ["1234", "5678", "9000"],
+};
+
+const process = {
+  login: (req, res) => {
+    const id = req.body.id,
+      psword = req.body.psword;
+
+    console.log(id, psword);
+
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.psword[idx] === psword) {
+        return res.json({
+          success: true,
+        });
+      }
+    }
+    return res.json({
+      sucess: false,
+      msg: "로그인에 실패하셨습니다.",
+    });
+  },
 };
 
 module.exports = {
-  hello,
-  login,
+  output,
+  process,
 };
